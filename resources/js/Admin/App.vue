44d49ef7c3680e4901b286a7a8b1fe0b430b2fac
Nakaoshi <89528377+Nakaoshi@users.noreply.github.com>
2022-05-11 21:21:36 -0300

    first commit

---
+++ resources/js/Admin/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Cliente/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from '../layout/template-container/index.vue'
+export default {
+    components:{
+        NavBar
+    }
+}
+</script>
+
+<style>
+
+</style>
