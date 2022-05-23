846f251b95a000ecb9d68b002775e5e46a037386
Nakaoshi <89528377+Nakaoshi@users.noreply.github.com>
2022-05-23 17:11:04 -0300

    navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,36 +1,18 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue').default;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+
 
 // const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+//  files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
  import Vue from 'vue'
- import App from './view/App.vue'
+ import App from './App.vue'
  import VueRouter from 'vue-router'
  import routes from './routes'
- import Vuetify from './plugins/vuetify'
+ import Vuetify from '../plugins/vuetify'
  
  // import './plugins/validation/vee-validate.js'
  // import './plugins/vuetify-money.js'
@@ -39,6 +21,7 @@ window.Vue = require('vue').default;
  import VueSweetalert2 from 'vue-sweetalert2'
  import 'sweetalert2/dist/sweetalert2.min.css'
  require('./bootstrap');
+
  
  // ---------------------------------------------------------------------------------------------------------------------/
  // Vuex
@@ -51,33 +34,8 @@ window.Vue = require('vue').default;
  import axios from 'axios'
  
  Vue.use(VueSweetalert2)
- Vue.component('app', App)
-// Add a request interceptor
-//  axios.interceptors.request.use(function (config) {
-//    if (!config.headers['No-Loading']) {
-//      store.commit('loadingScreen/openLoading')
-//    }
-//
-//    const token = store.getters['auth/isAuthenticated']
-//
-//    if (token) {
-//      config.headers.Authorization = `Bearer ${token}`
-//    }
-//
-//    return config
-//  })
-//
-// Add a response interceptor
-//  axios.interceptors.response.use(
-//    function (response) {
-//      store.commit('loadingScreen/closeLoading')
-//      return response
-//    },
-//    function (error) {
-//      store.commit('loadingScreen/closeLoading')
-//      return Promise.reject(error)
-//    }
-//  )
+
+
  
  Vue.prototype.$axios = axios
  
@@ -95,32 +53,11 @@ window.Vue = require('vue').default;
    }
  });
  
-//  router.beforeEach((to, from, next) => {
-//    if (to.matched.some((record) => record.meta.forVisitors)) {
-//      if (store.getters['auth/isAuthenticated']) {
-//        if (store.getters['user/getUser'].perfil === 'agente de atendimento') {
-//          next({
-//            path: '/admin/orcamentos'
-//          })
-//        } else {
-//          next({
-//            path: '/admin/clientes'
-//          })
-//        }
-//      } else next()
-//    } else if (to.matched.some((record) => record.meta.forAuth)) {
-//      if (!store.getters['auth/isAuthenticated']) {
-//        next({
-//          path: '/admin/'
-//        })
-//      } else next()
-//    } else next()
-//  })
  
  // ---------------------------------------------------------------------------------------------------------------------/
- // Vue
- // ------- --------------------------------------------------------------------------------------------------------------/
-  new Vue({
+ // Vue 
+ // ---------------------------------------------------------------------------------------------------------------------/
+ new Vue({
    el: '#app',
    router,
    // store,
--- resources/js/view/Inicio.vue
+++ resources/js/view/Inicio.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
+  <div id="app">
+  <v-parallax src="../../css/images/Banner_Index.png" class="relative">
+  <p class="banner--text">DO THE <span color="rosa">IMPOSSIBLE</span></p>
+  </v-parallax>
+
+  <h1>sim</h1>
+
   </div>
 </template>
 
 <script>
+import inicioIndex from '../components/Inicio.vue'
 export default {
-
+  name:'inicio',
+    components:{
+        inicioIndex
+    }
 }
 </script>
 
