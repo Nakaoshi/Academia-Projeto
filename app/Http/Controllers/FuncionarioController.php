3f44eb50371d09599c469bfcbec964b7bbf591db
Rodrigo Veras <rodrigoveras2009@hotmail.com>
2022-09-29 17:29:20 -0300

    Inclusão dos controllers e config da routes api

---
+++ app/Http/Controllers/FuncionarioController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Funcionario;
+
+class FuncionarioController extends Controller
+{
+    public function index()
+    {
+        $funcionarios = Funcionario::all()->toArray();
+        return array_reverse($funcionarios);      
+    }
+    public function createFuncionario(Request $request)
+    {
+        $funcionario = new Funcionario([
+            'nome' => $request->input('nome'),
+            'sobrenome'=> $request->input('sobrenome'),
+            'cpf'=> $request->input('cpf'),
+            'genero'=> $request->input('genero'),
+            'cargo'=> $request->input('cargo'),
+            'salario'=> $request->input('salario'),
+            'telefone'=> $request->input('telefone'),
+            'email'=> $request->input('email')
+            
+        ]);
+        $funcionario->save();
+        return response()->json('funcionario Criado');
+    }
+    public function showFuncionario($id)
+    {
+        $funcionario = Funcionario::find($id);
+        return response()->json($funcionario);
+    }
+    public function updateFuncionario($id, Request $request)
+    {
+        $funcionario = Funcionario::find($id);
+        $funcionario->update($request->all());
+        return response()->json('funcionario Atualizado');
+    }
+    public function deleteFuncionario($id)
+    {
+        $funcionario = Funcionario::find($id);
+        $funcionario->delete();
+        return response()->json('funcionario deletado');
+    }
+}
